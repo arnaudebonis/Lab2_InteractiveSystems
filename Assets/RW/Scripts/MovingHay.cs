3ea80ae50aece69dbc96668b9762088c8c39bd80
arnaudebonis <39151496+arnaudebonis@users.noreply.github.com>
2020-04-21 15:21:46 +0200

    colisions

---
+++ Assets/RW/Scripts/MovingHay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingHay : MonoBehaviour
+{
+    public float limitHayMov = 21;
+    public float horizontalBoundary = 22;
+    public GameObject hayBalePrefab; // 1
+    public Transform haySpawnpoint; // 2
+    public float shootInterval; // 3
+    private float shootTimer; // 4
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        UpdateMovement();
+        UpdateShooting();
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // 1
+
+        if(horizontalInput < 0 && transform.position.x > -horizontalBoundary) // 1
+{
+            transform.Translate(transform.right * -limitHayMov * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary) // 2
+        {
+            transform.Translate(transform.right * limitHayMov * Time.deltaTime);
+        }
+    }
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime; // 1
+
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space)) // 2
+        {
+            shootTimer = shootInterval; // 3
+            ShootHay(); // 4
+        }
+    }
+
+    private void ShootHay()
+    {
+        SoundManager.Instance.PlayShootClip();
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+}
